dc699e424bc043f33488ffc06721093ef5c772b4
LucasYokoy <lucas.yokoy@gmail.com>
2021-08-11 02:38:41 -0300

    checkpoints 2 and 3 completed

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.6.0 <0.7.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.6.0 <0.9.0;
 
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
@@ -8,32 +9,75 @@ contract Staker {
   // Edition for checkpoint 0
 
   mapping ( address => uint256 ) public balances;
-  uint256 public constant treshold = 1 ether;
+  uint256 public constant threshold = 1 ether;
+  uint256 public deadline;
 
+  address payable exampleExternalContractAddress;
   ExampleExternalContract public exampleExternalContract;
 
-  constructor(address exampleExternalContractAddress) public {
+  constructor(address payable _exampleExternalContractAddress) {
     exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+    exampleExternalContractAddress = _exampleExternalContractAddress;
+    deadline = block.timestamp + 30 seconds;
+  }
+
+  // Modifier that makes sure the function is only executed after the deadline has passed
+  modifier afterDeadline{
+    require(block.timestamp >= deadline, "Can't do that before the deadline");
+    _;
+  }
+
+  // Modifier that makes sure the external contract hasn't been completed yet. Used to protect execute and witdraw functions
+  modifier notCompleted{
+    require(!exampleExternalContract.completed());
+    _;
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
   event Stake(address, uint256);
 
-  function stake() payable{
-    //
+  function stake() external payable{
+    // check if the deadline hasn't passed
+    require(block.timestamp < deadline, "Can't stake after deadline.");
+    // Update balance
+    balances[msg.sender] = msg.value;
+
+    // Emit Stake event
+    emit Stake(msg.sender, msg.value);
   }
 
   // After some `deadline` allow anyone to call an `execute()` function
   //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
-
+  function execute() external afterDeadline notCompleted payable{
+    // make sure the deadline has passed (modifier)
+    // make sure the threshold was reached
+    require(address(this).balance >= threshold, "Failed to reach the threshold");
+    // Call the complete() function from the external contract
+    // Transfer funds to external contract
+    exampleExternalContract.complete{value: address(this).balance}();
+    // exampleExternalContractAddress.transfer(address(this).balance);
+  }
 
 
   // if the `threshold` was not met, allow everyone to call a `withdraw()` function
-
+  function withdraw(address payable user) external afterDeadline notCompleted payable{
+    // make sure the deadline has passed (modifier)
+    // make sure the threshold wasn't reached
+    require(address(this).balance < threshold, "Can't withdraw if the threshold was reached");
+    // transfer the sender's balance back to the sender
+    user.transfer(balances[user]);
+  }
 
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
+  function timeLeft() public view returns(uint256){
+    uint256 time = deadline - block.timestamp;
+    if(time >= 0){
+      return time;
+    } else{
+      return 0;
+    }
+  }
 
 }
